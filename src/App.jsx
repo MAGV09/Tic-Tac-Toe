fa58efaeb8b692ebdca74144f5d4ed65129d3317
MAGV09 <mohamedadel2002320@gmail.com>
2025-11-20 08:08:48 +0200

    refactor winning patterns logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import './App.css';
-import grids from './data.js';
+import grids from '../utils/data.js';
 import Scoreboard from './components/Scoreboard.jsx';
+import winningPatterns from '../utils/gameLogic.js';
 function App() {
   const [value, setValue] = useState('X');
   const [scoreboard, setScore] = useState({
@@ -10,41 +11,8 @@ function App() {
     roundCount: 1,
   });
   const [winner, setWinner] = useState('');
-  function checkPattern(grids,val) {
-    const case1 =
-      grids[0].value === val &&
-      grids[1].value === val &&
-      grids[2].value === val;
-    const case2 =
-      grids[3].value === val &&
-      grids[4].value === val &&
-      grids[5].value === val;
-    const case3 =
-      grids[6].value === val &&
-      grids[7].value === val &&
-      grids[8].value === val;
-    const case4 =
-      grids[0].value === val &&
-      grids[4].value === val &&
-      grids[8].value === val;
-    const case5 =
-      grids[2].value === val &&
-      grids[4].value === val &&
-      grids[6].value === val;
-    const case6 =
-      grids[0].value === val &&
-      grids[3].value === val &&
-      grids[6].value === val;
-    const case7 =
-      grids[1].value === val &&
-      grids[4].value === val &&
-      grids[7].value === val;
-    const case8 =
-      grids[2].value === val &&
-      grids[5].value === val &&
-      grids[8].value === val;
-
-    const winningPatern = [case1, case2, case3, case4, case5, case6, case7, case8];
+  function checkPattern(grids, val) {
+    const winningPatern = winningPatterns(grids, val);
 
     winningPatern.forEach((item) => {
       if (item) {
@@ -91,8 +59,8 @@ function App() {
       grid.value = value;
       setValue(value === 'X' ? 'O' : 'X');
     }
-    checkPattern(grids,'X');
-    checkPattern(grids,'O');
+    checkPattern(grids, 'X');
+    checkPattern(grids, 'O');
     draw();
   }
   return (
